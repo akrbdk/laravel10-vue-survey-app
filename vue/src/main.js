8e539a902ebf72961ee240a536955824b92ec23e
alexander.karabudka <sanyakarabudka@gmail.com>
2023-08-02 16:27:46 +0300

    setup tailwindcss

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
 import store from "./store";
+import './input.css'
+import App from './App.vue'
 
 createApp(App).use(store).mount('#app')
